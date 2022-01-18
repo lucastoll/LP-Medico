f53ff502193d2ed35efe63930a43b2e3ed5b110b
lucastoll <lucasfernandestolotto@gmail.com>
2022-01-18 16:42:16 -0300

    home + header completos
    home e header completos e responsivos
    formulário funcionando
    popup formulario funcionando

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -1,3 +1,5 @@
+/* Navbar mobile */
+
 const botaoMenu = document.querySelector('[data-menu-button]');
 const divListaMenu = document.querySelector('[data-lista-menu]');
 let controleBotaoMenu = 0;
@@ -21,3 +23,32 @@ else
      controleBotaoMenu = 1;
 }
 })
+
+/* Formulário / popUp*/
+
+const popup = document.querySelector('.popUp')
+
+popup.addEventListener('click', (e) => {
+  if(e.target.id == 'popUp__fechar1'|| e.target.id == 'popUp__fechar2')
+    popup.style.display = 'none';
+})
+
+
+const myForm = document.querySelector("[data-form]")
+
+myForm.addEventListener("submit", (e) => {
+     e.preventDefault();
+     popup.style.display = 'flex';
+
+     const paragrafo = document.querySelector('[data-popUp-paragrafo]')
+     const departamento = document.querySelector('[data-form-department]').value
+     const time = document.querySelector('[data-form-time]').value
+
+     paragrafo.textContent = `We will send you an email with options for ${departamento} clinics with free space on the agenda at ${time}.`
+
+     document.querySelector('[data-form-department]').value = ""
+     document.querySelector('[data-form-time]').value = ""
+     document.querySelector('[data-form-name]').value = ""
+     document.querySelector('[data-form-email]').value = ""
+ }) 
+
