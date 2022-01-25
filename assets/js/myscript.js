59b5b94a4466e6558ea3b345a976cf1f63ac6379
lucastoll <lucasfernandestolotto@gmail.com>
2022-01-25 15:17:30 -0300

    section 2 pronta
    section 2 pronta
    
    animação de entrada + hover + responsividade

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -52,3 +52,34 @@ myForm.addEventListener("submit", (e) => {
      document.querySelector('[data-form-email]').value = ""
  }) 
 
+/* Animações caseBox */
+const animaCaseBox = function revealCaseBox () {
+     let cases = document.getElementsByClassName('cases__caseBox')
+     var windowHeight = window.innerHeight; /* height da pagina */
+     var elementTop = cases[0].getBoundingClientRect().top;
+     var elementVisible = 100;
+
+
+     console.log(windowHeight + "," + elementTop + "," + elementVisible)
+     for(var i = 0; i < cases.length; i++)
+     {
+          var elementTop = cases[i].getBoundingClientRect().top;
+          if(elementTop < windowHeight - elementVisible) /* distancia ate o topo < altura da pag - altura da revelação */
+          {                                         
+               for(var l = 0; l < cases.length; l++)
+               {
+                    cases[i].classList.remove("cases__caseBox--invisivel")
+               }
+          } 
+     }
+
+     if(cases[2].getBoundingClientRect().top < windowHeight - elementVisible)
+     {
+          for(var j = 0; j < cases.length; j++)
+          {
+               cases[j].style.transition = "all .3s"
+          }
+     }
+}
+
+window.addEventListener("scroll", animaCaseBox); 
